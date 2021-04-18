d1d77ecbfecce3a4f5c3a36d80c7ccbac747638f
Yoshihiro Tanaka <contact@cordea.jp>
2021-04-18 12:40:35 +0900

    receive user input

--- src/main.lua
+++ src/main.lua
@@ -5,6 +5,14 @@ Shell = require("shell")
 Quick = require("quick")
 
 function main()
+    local input = io.read()
+    local list = {}
+    for v in string.gmatch(input, "(%d+)") do
+        table.insert(list, tonumber(v))
+    end
+    local bubble = Bubble:new({ list = list })
+    bubble:sort()
+    print(table.concat(list, " "))
 end
 
 main()
